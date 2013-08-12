9bf78a6dd0ce7af2af6bbef133a45fde437b306a
Vladimir Kornev <vkornev@thumbtack.net>
2013-08-12 18:39:57 +0700

    Create project nosql-research

--- nosql-research/src/main/java/net/thumbtack/research/nosql/Configurator.java
+++ nosql-research/src/main/java/net/thumbtack/research/nosql/Configurator.java
@@ -1,5 +1,12 @@
 package net.thumbtack.research.nosql;
 
+import com.netflix.config.ConcurrentMapConfiguration;
+import com.netflix.config.DynamicPropertyFactory;
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.PropertiesConfiguration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * User: vkornev
  * Date: 12.08.13
@@ -8,4 +15,16 @@ package net.thumbtack.research.nosql;
  * Class for work with configs
  */
 public class Configurator {
+    private static final Logger log = LoggerFactory.getLogger(Configurator.class);
+    private final ConcurrentMapConfiguration propertyFactory;
+
+    public Configurator(String fileName) {
+        try {
+            propertyFactory = new ConcurrentMapConfiguration(new PropertiesConfiguration(fileName));
+        } catch (ConfigurationException e) {
+            e.printStackTrace();
+            log.error(e.getMessage());
+            throw new RuntimeException(e);
+        }
+    }
 }
